2d6dfc149e64680c588e4e839824dc5f76689758
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-03 09:17:54 -0700

    Pass test for Cart()

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic for Cart constructor
+function Cart() {
+  this.items = {};
+  this.currentId = 0;
+}
+
+Cart.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Cart.prototype.addItem = function(item) {
+  item.id = this.assignId();
+  this.items[item.id] = item;
+};
+
